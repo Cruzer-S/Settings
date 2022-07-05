df6e22e07bf83f0e3effc03ddf7c1873d9d9b479
Cruzer-S <yyyynoom@gmail.com>
2022-07-05 14:17:11 +0900

    Neovim (RaspberryPi): CocInstall Synchronize, clangd 패키지 설치 구문 추가

--- Neovim/RaspberryPi/install.sh
+++ Neovim/RaspberryPi/install.sh
@@ -6,10 +6,13 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 mkdir -p $HOME/.config/nvim
 cp ../init.vim $HOME/.config/nvim/init.vim
 
-sudo apt install libstdc++-10-dev
+sudo apt install -y libstdc++-10-dev
+sudo apt install -y clangd
 curl -sL install-node.now.sh/lts | sudo $SHELL -s -- --yes
 
-nvim +:PlugInstall +:qa
-nvim +:CocInstall coc-clangd +:qa
-nvim +:CocCommand clangd.install +:qa
-nvim +:TSUpdateSync +:qa
+nvim "+:PlugInstall" "+:qa"
+nvim "+:CocInstall -sync coc-clangd" "+:qa"
+nvim "+:CocInstall -sync coc-json" "+:qa"
+nvim "+:CocInstall -sync coc-pyright" "+:qa"
+# nvim "+:CocCommand clangd.install" "+:qa" dummy.c
+nvim "+:TSUpdateSync" "+:qa"
