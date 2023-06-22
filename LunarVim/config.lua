aa1f612c06519340110487477630cb753da4511d
mythos <yyyynoom@gmail.com>
2023-06-23 03:09:00 +0900

    LunarVim Config 파일 수정

--- LunarVim/config.lua
+++ LunarVim/config.lua
@@ -3,3 +3,9 @@ lvim.keys.insert_mode["kj"] = "<ESC>"
 
 vim.opt.tabstop = 8
 vim.opt.shiftwidth = 8
+vim.opt.expandtab = false
+
+lvim.keys.normal_mode["<S-x>"] = ":BufferKill<CR>"
+
+lvim.keys.normal_mode["<S-h>"] = ":bprev<CR>"
+lvim.keys.normal_mode["<S-l>"] = ":bnext<CR>"
