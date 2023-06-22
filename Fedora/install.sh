1c946158a09f77c065a50a43028f30e0b48fbb06
mythos <yyyynoom@gmail.com>
2023-06-23 03:46:31 +0900

    Fedora gnome 관련 추가 패키지 설치 & 불필요한 기존 패키지 제거

--- Fedora/install.sh
+++ Fedora/install.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 sudo dnf install -y gh bat google-chrome-stable
+sudo dnf install -y gnome-extensions gnome-tweaks
+sudo dnf remove -y gnome-tour
 
 echo "alias cat='bat'" >> ~/.bashrc
 
