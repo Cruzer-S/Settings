7e32f2d3127e19d6b1736c476406c15470f09a2a
Cruzer-S <yyyynoom@gmail.com>
2023-06-28 17:49:19 +0900

    telescope 와 toggleterm 설정 추가

--- LunarVim/config.lua
+++ LunarVim/config.lua
@@ -1,14 +1,55 @@
+--------------------------------------------------------------
+-- general settings
+--------------------------------------------------------------
+lvim.colorscheme = "slate"
+
 lvim.keys.insert_mode["jk"] = "<ESC>"
 lvim.keys.insert_mode["kj"] = "<ESC>"
 
+lvim.keys.normal_mode["<S-x>"] = ":BufferKill<CR>"
+lvim.keys.normal_mode["<S-h>"] = ":bprev<CR>"
+lvim.keys.normal_mode["<S-l>"] = ":bnext<CR>"
+
+-- vim settings
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = false
 
-lvim.keys.normal_mode["<S-x>"] = ":BufferKill<CR>"
-
-lvim.keys.normal_mode["<S-h>"] = ":bprev<CR>"
-lvim.keys.normal_mode["<S-l>"] = ":bnext<CR>"
-
+--------------------------------------------------------------
+-- telescope settings
+--------------------------------------------------------------
+local _, actions = pcall(require, "telescope.actions")
+lvim.builtin.telescope.defaults.mappings = {
+  -- for input mode
+  i = {
+    ["<C-j>"] = actions.move_selection_next,
+    ["<C-k>"] = actions.move_selection_previous,
+    ["<C-n>"] = actions.cycle_history_next,
+    ["<C-p>"] = actions.cycle_history_prev,
+  },
+  -- for normal mode
+  n = {
+    ["<C-j>"] = actions.move_selection_next,
+    ["<C-k>"] = actions.move_selection_previous,
+  },
+}
+--------------------------------------------------------------
+-- toggleterm settings
+--------------------------------------------------------------
 lvim.builtin.terminal.direction = "horizontal"
 lvim.builtin.terminal.size = 10
+
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<ESC>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', 'kj', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()')
